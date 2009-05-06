99025abdff2d2a93ef92142fa8cc4209d2be8e78
jsvazic <jsvazic@3cabb256-354f-11de-9e80-b52b74bdfc52>
2009-05-06 03:21:51 +0000

    Reverted the changes to generateInitialPopulation(), adjusted some properties and added some timing statistics.

--- src/com/arm/fanucci/FanucciCalc.java
+++ src/com/arm/fanucci/FanucciCalc.java
@@ -11,7 +11,7 @@ import com.arm.genetic.Chromosome;
  */
 public class FanucciCalc {
 	/** The default size for the population. */
-	private static final int POPULATION_SIZE = 1024;
+	private static final int POPULATION_SIZE = 64;
 
 	/** The elitism rate for the simulation, where: 0.0 &lt; rate &lt; 1.0 */
 	private static final float ELITISM_RATE = 0.1f;
@@ -20,7 +20,7 @@ public class FanucciCalc {
 	private static final float MUTATION_RATE = 0.15f;
 	
 	/** Maximum number of iterations for the simulation. */
-	private static final int MAX_ITERATION = 128;
+	private static final int MAX_ITERATION = 64;
 	
 	private static final int MAX_HANDS = 4;
 
@@ -30,23 +30,31 @@ public class FanucciCalc {
 	 * @param args Command line argument (ignored).
 	 */
 	public static void main(String[] args) throws Exception {
+		long startTime = System.currentTimeMillis();
 		Importer importer = new Importer();
 		importer.importCards(new File("etc/cards.xml"));
-
+		Chromosome[] arr = new Chromosome[4];
 		for (int j = 0; j < MAX_HANDS; j++) {
 			FanucciPopulation population = new FanucciPopulation(
 					POPULATION_SIZE, importer);
 
-			Chromosome bestChromosome = population.getBestChromosome();
-			for (int i = 1; i <= MAX_ITERATION; i++) {
+			for (int i = 0; i < MAX_ITERATION; i++) {
 				// Evolve the population
 				population.evolve(ELITISM_RATE, MUTATION_RATE);
 			}
-		
-			bestChromosome.printGene();
-			for (Card c : ((FanucciChromosome) bestChromosome).getCards()) {
+			
+			arr[j] = population.getBestChromosome();
+			for (Card c : ((FanucciChromosome) arr[j]).getCards()) {
 				importer.removeCard(c);
 			}
 		}
+		long endTime = System.currentTimeMillis();
+		
+		for (Chromosome c : arr) {
+			c.printGene();
+		}
+		
+		System.out.println();
+		System.out.println("Total time: " + (endTime - startTime) + "ms");
 	}
 }
