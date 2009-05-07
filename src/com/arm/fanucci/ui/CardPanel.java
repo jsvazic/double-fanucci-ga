91121e425e16c265591b7adee2fd182c7fb0673f
jsvazic <jsvazic@3cabb256-354f-11de-9e80-b52b74bdfc52>
2009-05-07 20:07:13 +0000

    Added support for images for the cards and included images for the bugs.

--- src/com/arm/fanucci/ui/CardPanel.java
+++ src/com/arm/fanucci/ui/CardPanel.java
@@ -1,13 +1,24 @@
 package com.arm.fanucci.ui;
 
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,6 +35,8 @@ public class CardPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
+	private static final String BLANK = "blank_panel";
+	
 	private static final String[] SUITS = {
 			"Bugs", "Lamps", "Mazes", "Fromps", "Hives", "Inkblots", "Ears",
 			"Time", "Scythes", "Zurfs", "Books", "Plungers", "Tops", "Rain",
@@ -31,11 +44,12 @@ public class CardPanel extends JPanel {
 	};
 	
 	private static final String[] BUTTON_LABELS = {
-			"Naught", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Infinity"
+			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\u221E"
 	};
 	
 	private JList suitList;
 	private JPanel buttonPanel;
+	private CardLayout cardLayout;
 	private Deck deck;
 
 	/**
@@ -53,25 +67,78 @@ public class CardPanel extends JPanel {
 		setLayout(new GridBagLayout());
 		suitList = new JList(SUITS);
 		suitList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		suitList.setSelectedIndex(0);
 		add(new JScrollPane(suitList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
 					JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), 
 					new GridBagConstraints(0, 0, 1, 1, 0.3, 0.3, 
 							GridBagConstraints.WEST, GridBagConstraints.BOTH,
 							new Insets(1, 1, 1, 1), 0, 0)
 		);
-		
+
 		buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(4, 3, 5, 5));
-		for (String label : BUTTON_LABELS) {
-			JToggleButton button = new JToggleButton(label);
-			button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					handleButtonToggled(e);
+		cardLayout  = new CardLayout();
+		
+		buttonPanel.setLayout(cardLayout);
+		buttonPanel.add(new JPanel(), BLANK);
+		
+		// Iterate over the suits, giving a custom panel for each.
+		for (String suit : SUITS) {
+			JPanel innerPanel = new JPanel();
+			innerPanel.setLayout(new GridBagLayout());
+			
+			BufferedImage img = null;
+			BufferedImage selectedImg = null;
+			try {
+				File imgFile = new File("images/" + suit.toLowerCase() + 
+						".gif");
+				if (imgFile.exists() && imgFile.canRead()) {
+					img = ImageIO.read(imgFile);
 				}
-			});
-			buttonPanel.add(button);
+				
+				imgFile = new File("images/" + suit.toLowerCase() + 
+						"_selected.gif");
+				if (imgFile.exists() && imgFile.canRead()) {
+					selectedImg = ImageIO.read(imgFile);
+				}
+				
+			} catch (IOException ex) {
+				//ex.printStackTrace();
+			}
+		
+			int xPos = 0;
+			int yPos = 0;
+			for (String label : BUTTON_LABELS) {
+				JToggleButton button;
+				if (img != null && selectedImg != null) {
+					button = new JToggleButton(new FanucciCardImageIcon(
+							img, label));
+					
+					button.setSelectedIcon(new FanucciCardImageIcon(
+							selectedImg, label));
+				} else {
+					button = new JToggleButton(label);
+				}
+				
+				button.setMargin(new Insets(0, 0, 0, 0));
+				button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						handleButtonToggled(e);
+					}
+				});
+			
+				innerPanel.add(button, new GridBagConstraints(
+						xPos, yPos, 1, 1, 0.5, 0.5, GridBagConstraints.CENTER, 
+						GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 
+						1, 1));
+			
+				xPos = (xPos + 1) % 3;
+				if (xPos == 0) {
+					++yPos;
+				}
+			}
+			
+			buttonPanel.add(innerPanel, suit);
 		}
+		
 		add(new JScrollPane(buttonPanel, 
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED),
@@ -79,8 +146,11 @@ public class CardPanel extends JPanel {
 						GridBagConstraints.CENTER, GridBagConstraints.BOTH, 
 						new Insets(1, 1, 1, 1), 0, 0)
 		);
-		
+	
 		addListeners();
+		
+		// Set a default selection for the suits
+		suitList.setSelectedIndex(0);
 	}
 	
 	/**
@@ -100,37 +170,10 @@ public class CardPanel extends JPanel {
 					return;
 				}
 				if (suitList.getSelectedIndex() == -1) {
-					// Disable the buttons
-					int count = buttonPanel.getComponentCount();
-					for (int i = 0; i < count; i++) {
-						AbstractButton button = 
-								(AbstractButton) buttonPanel.getComponent(i);
-						
-						button.setSelected(false);
-						button.setEnabled(false);
-					}
+					cardLayout.show(buttonPanel, BLANK);
 				} else {
-					// Enable the buttons.
-					int count = buttonPanel.getComponentCount();
-					for (int i = 0; i < count; i++) {
-						AbstractButton button = 
-								(AbstractButton) buttonPanel.getComponent(i);
-												
-						// Determine if the button should be enabled or not.
-						short suitId = FanucciUtil.getSuitId(
-								(String) suitList.getSelectedValue());
-						
-						short groupId = FanucciUtil.getGroupId(suitId);
-						short value = FanucciUtil.getValue(button.getText());
-						
-						Card c = new Card(groupId, suitId, value);
-						if (deck.hasCard(c)) {
-							button.setSelected(true);
-						} else {
-							button.setSelected(false);
-						}
-						button.setEnabled(true);
-					}
+					cardLayout.show(buttonPanel, 
+							(String) suitList.getSelectedValue());
 				}
 			}			
 		});		
@@ -154,6 +197,42 @@ public class CardPanel extends JPanel {
 			deck.addCard(c);
 		} else {
 			deck.removeCard(c);
+		}
+	}
+	
+	/**
+	 * Class used for drawing text onto images.
+	 * 
+	 * @author jsvazic
+	 */
+	private class FanucciCardImageIcon extends ImageIcon {
+		private static final long serialVersionUID = 1L;
+		private Font font;
+		private String label;
+		
+		/**
+		 * Default constructor.
+		 * 
+		 * @param img The image to use for the icon.
+		 * @param label The label for the card.
+		 */
+		public FanucciCardImageIcon(Image img, String label) {
+			super(img);
+			this.font  = new Font("Verdana", Font.PLAIN, 12);
+			this.label = label;
+		}
+		
+		@Override
+		public void paintIcon(Component c, Graphics g, int x, int y) {
+			super.paintIcon(c, g, x, y);
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setFont(font);
+			g2d.setColor(Color.BLACK);
+			g2d.drawString(label, 5, 15);
+			g2d.translate(c.getWidth() - 5, c.getHeight() - 15);			
+			g2d.rotate(Math.PI); // Rotate 180 degrees
+			g2d.drawString(label, 0, 0);
+			g2d.translate(c.getWidth() - 5, c.getHeight() - 15);			
 		}
 	}
 }
