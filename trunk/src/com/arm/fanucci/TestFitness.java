52778d71e2ea4b2a340e4d7d9b9c327c9190a80b
jsvazic <jsvazic@3cabb256-354f-11de-9e80-b52b74bdfc52>
2009-05-11 19:26:02 +0000

    Some more minor fixes, but not quite there yet.

--- trunk/src/com/arm/fanucci/TestFitness.java
+++ trunk/src/com/arm/fanucci/TestFitness.java
@@ -8,35 +8,19 @@ public class TestFitness implements IFanucci {
 	public static void main(String[] args) {
 		Set<Card> cards = new TreeSet<Card>();
 		cards.add(new Card(FanucciUtil.getGroupId(SUIT_TOPS), SUIT_TOPS, 
-				POWER_THREE));
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_TOPS), SUIT_TOPS, 
 				POWER_TWO));
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_EARS), SUIT_EARS, 
+		cards.add(new Card(FanucciUtil.getGroupId(SUIT_TOPS), SUIT_TOPS, 
+				POWER_ONE));
+		cards.add(new Card(FanucciUtil.getGroupId(SUIT_MAZES), SUIT_MAZES, 
 				POWER_TWO));
 		cards.add(new Card(FanucciUtil.getGroupId(SUIT_FROMPS), SUIT_FROMPS, 
-				POWER_TWO));
+				POWER_ONE));
 		
 		FanucciChromosome chromosome = new FanucciChromosome(
 				new FanucciPopulation(new Card[0], 0), cards);
 		
 		System.out.println("Fitness: " + (100 - chromosome.getFitness()));
 		System.out.println("--------------------------------");
-
-		cards.clear();
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_BUGS), SUIT_BUGS, 
-				POWER_TWO));
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_BUGS), SUIT_BUGS, 
-				POWER_ONE));
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_ZURFS), SUIT_ZURFS, 
-				POWER_ONE));
-		cards.add(new Card(FanucciUtil.getGroupId(SUIT_INKBLOTS), SUIT_INKBLOTS, 
-				POWER_TWO));
-		
-		chromosome = new FanucciChromosome(new FanucciPopulation(
-				new Card[0], 0), cards);
-		
-		System.out.println("Fitness: " + (100 - chromosome.getFitness()));
-
 	}
 
 }
