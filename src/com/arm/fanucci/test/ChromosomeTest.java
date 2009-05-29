faeab794caac42c7f88aca902bb5cac9be399a8b
jsvazic <jsvazic@3cabb256-354f-11de-9e80-b52b74bdfc52>
2009-05-29 03:28:54 +0000

    Minor algorithm update to have fitness measured in absolute value compared to 100, instead of just greater than or equal to 100 as being optimal.

--- src/com/arm/fanucci/test/ChromosomeTest.java
+++ src/com/arm/fanucci/test/ChromosomeTest.java
@@ -18,8 +18,7 @@ public class ChromosomeTest implements IFanucci {
 		set.add(new Card(SUIT_TOPS, POWER_FIVE));
 		set.add(new Card(SUIT_TOPS, POWER_FOUR));		
 		Chromosome c = new Chromosome(null, set);
-		System.out.println("Expecting 62: " + 
-				formatter.format(100.0 - c.getFitness()));
+		System.out.println("Expecting 62: " + formatter.format(c.getScore()));
 		
 		set.clear();
 		set.add(new Card(SUIT_MAZES, POWER_FIVE));
@@ -27,8 +26,7 @@ public class ChromosomeTest implements IFanucci {
 		set.add(new Card(SUIT_EARS, POWER_THREE));
 		set.add(new Card(SUIT_TOPS, POWER_FIVE));
 		c = new Chromosome(null, set);
-		System.out.println("Expecting 72: " + 
-				formatter.format(100.0 - c.getFitness()));
+		System.out.println("Expecting 72: " + formatter.format(c.getScore()));
 
 		set.clear();
 		set.add(new Card(SUIT_TOPS, POWER_THREE));
@@ -36,8 +34,7 @@ public class ChromosomeTest implements IFanucci {
 		set.add(new Card(SUIT_MAZES, POWER_THREE));
 		set.add(new Card(SUIT_TOPS, POWER_FOUR));
 		c = new Chromosome(null, set);
-		System.out.println("Expecting 48: " + 
-				formatter.format(100.0 - c.getFitness()));
+		System.out.println("Expecting 48: " + formatter.format(c.getScore()));
 
 		set.clear();
 		set.add(new Card(SUIT_BUGS, POWER_TWO));
@@ -45,8 +42,7 @@ public class ChromosomeTest implements IFanucci {
 		set.add(new Card(SUIT_TIME, POWER_TWO));
 		set.add(new Card(SUIT_TIME, POWER_ONE));
 		c = new Chromosome(null, set);
-		System.out.println("Expecting 24: " + 
-				formatter.format(100.0 - c.getFitness()));
+		System.out.println("Expecting 24: " + formatter.format(c.getScore()));
 
 		set.clear();
 		set.add(new Card(SUIT_BUGS, POWER_THREE));
@@ -54,7 +50,6 @@ public class ChromosomeTest implements IFanucci {
 		set.add(new Card(SUIT_TIME, POWER_THREE));
 		set.add(new Card(SUIT_TIME, POWER_FOUR));
 		c = new Chromosome(null, set);
-		System.out.println("Expecting 72: " + 
-				formatter.format(100.0 - c.getFitness()));
+		System.out.println("Expecting 72: " + formatter.format(c.getScore()));
 	}
 }
